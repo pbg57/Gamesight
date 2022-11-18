78bcd41aa841830f13e131ad893c95de7716cd47
Phil Griffin <pbg57@yahoo.com>
2022-11-18 12:13:12 -0700

    Reorg repo paging/sorting tests

--- src/main/java/org/gamesight/InitializingBeanGamesight.java
+++ src/main/java/org/gamesight/InitializingBeanGamesight.java
@@ -30,8 +30,11 @@ public class InitializingBeanGamesight implements InitializingBean {
 	Placeholder class for application initialization actions.
 	 */
 	PlayerRepository playerRepository;
+
 	GameRepository gameRepository;
+
 	ProfileRepository profileRepository;
+
 	UserRepository userRepository;
 
 	private static final Logger logger = LogManager.getLogger(InitializingBeanGamesight.class);
@@ -47,96 +50,6 @@ public class InitializingBeanGamesight implements InitializingBean {
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		logger.debug("Hello from Log4j 2 - num : {}", () -> "InitializingBeanGamesight");
-
-//
-		//  Initial test data loaded during application start up.
-//		User user1 = new User();
-//		Profile profile1 = new Profile("6620 Fairways Drive", "Longmont", "CO", 80503, LocalDate.of(1957, Month.NOVEMBER, 29));
-//		user1.setProfile(profile1);
-//		profileRepository.save(profile1);
-//		userRepository.save(user1);
-
-		Profile profile;
-		for (int i=0; i<9; i++) {
-			// Note: repeated repo.save(profile) operations on same object are an 'update' action, not a 'create' action, of course.
-			profile = new Profile("2200 Fairways Drive", "Longmont", "CO", 80503+i, LocalDate.of(1957, Month.NOVEMBER, 29));
-			profileRepository.save(profile);
-		}
-
-		int pageNo = 0;
-		int pageSize =5;
-		String sortBy = "zip";
-		String sortDir = "ASC";
-
-		Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
-				: Sort.by(sortBy).descending();
-
-// create Pageable instance
-		Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
-		Page<Profile> profilePage = profileRepository.findAll(pageable);
-		logger.debug("Sorted Profile first zip: {} ", () -> profilePage.getContent().get(0).getZip());
-		logPageInfo(profilePage);
-		long pagedTotalElements = profilePage.getTotalElements();
-
-
-		if (profilePage.hasNext()) {
-			Pageable pageable2 = profilePage.nextPageable();
-			Page<Profile> profilePageNext = profileRepository.findAll(pageable2);
-			logPageInfo(profilePageNext);
-		}
-
-		List<Profile> profiles = profileRepository.findAll();
-		long findAllTotalElements = profiles.size();
-
-		logger.debug("Paged Profile size {}, Non-paged size {}", () -> pagedTotalElements, ()-> findAllTotalElements);
-
-
-
-//
-//
-//
-//		Player player = new Player("Dylan B.Griffin");
-//		Game game = new Game("WordleCheat", "Online");
-//		Game game2 = new Game("JeopardyCheat", "Streaming");
-//		player.getGames().add(game);
-//		player.getGames().add(game2);
-//		player.setUser(user1);
-//		game.getPlayers().add(player);
-//		game2.getPlayers().add(player);
-//		playerRepository.save(player);
-
-//		Player player2 = new Player("Jon O Griffin");
-//		Profile profile2 = new Profile("6620 Fairways Drive", "Longmont", "CO", 80503, LocalDate.of(1957, Month.NOVEMBER, 29));
-//		User user2 = new User();
-//		user2.setProfile(profile2);
-//		profile2.setUser(user2);
-//		player2.setUser(user2);
-//		player2.getGames().add(game);
-//		player2.getGames().add(game2);
-//		game.getPlayers().add(player2);
-//		game2.getPlayers().add(player);
-//		userRepository.save(user2);
-//		profileRepository.save(profile2);
-//		playerRepository.save(player2);
-
-	}
-
-	public static void logPageInfo(Page<Profile> profilePage) {
-		int number = profilePage.getNumber();
-		int numberOfElements = profilePage.getNumberOfElements();
-		int size = profilePage.getSize();
-		long totalElements = profilePage.getTotalElements();
-		int totalPages = profilePage.getTotalPages();
-		boolean hasNextPage = profilePage.hasNext();
-		logger.debug("Page info - page number {}, numberOfElements: {}, size: {}, "
-						+ "totalElements: {}, totalPages: {}, hasNextPage: {}",
-				number, numberOfElements, size, totalElements, totalPages, hasNextPage);
-
-		List<Profile> listOfProfiles = profilePage.getContent();
-
-		for(Profile p : listOfProfiles) {
-			logger.debug("Profile details: {}", () -> p.toString());
-		}
+		// Add any application startup processing here.
 	}
 }
---
+++ src/test/java/org/gamesight/test/standalone/RepositoryPagingAndSortingTests.java
@@ -0,0 +1,114 @@
+package org.gamesight.test.standalone;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.gamesight.model.Profile;
+import org.gamesight.repository.ProfileRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RepositoryPagingAndSortingTests {
+
+
+	@Autowired
+	ProfileRepository profileRepository;
+
+	private static final Logger logger = LogManager.getLogger(RepositoryPagingAndSortingTests.class);
+
+
+	@DisplayName("Test Profile entity CRUD operations via Java APIs")
+	@Test
+	void profilePaging() {
+
+		// Prepare for test by removing existing profiles.
+		List<Profile> profileList = profileRepository.findAll();
+		for (Profile p : profileList) {
+			profileRepository.deleteById(p.getId());
+		}
+		profileList = profileRepository.findAll();
+		assertEquals(0, profileList.size(), "Unable to remove all Profiles to begin testing.");
+
+		// Create multiple profiles for paging tests. The zip code field will differ in each new profile to allow sort testing.
+		Profile profile;
+		int profileCount = 10;
+		int lastZipNumber = 0;
+		for (int i = 0; i < profileCount; i++) {
+			// Note: repeated repo.save(profile) operations on same object are an 'update' action, not a 'create' action, of course.
+			lastZipNumber = 80503 + i;
+			profile = new Profile("2200 Fairways Drive", "Longmont", "CO", lastZipNumber, LocalDate.of(1957, Month.NOVEMBER, 29));
+			profileRepository.save(profile);
+		}
+
+		// Make sure we have the expected number of profiles:
+		List<Profile> profiles = profileRepository.findAll();
+		long findAllTotalElements = profiles.size();
+		assertEquals(findAllTotalElements, profileCount, "Wrong number of Profiles exist in DB at start of paging test.");
+
+		int pageNo = 0;		// Start on page zero, not page one !!!!
+		int pageSize = 5;
+		String sortBy = "zip";
+		String sortDir = "DESC";	// Profiles were just added with an ascending zip, so test with descending order.
+
+		Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+				: Sort.by(sortBy).descending();
+
+		// create Pageable instance
+		Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+		Page<Profile> profilePage = profileRepository.findAll(pageable);
+		int firstZipReturned = profilePage.getContent().get(0).getZip();
+		logger.debug("Sorted Profile first zip: {} ", () -> firstZipReturned);
+		// Test that the descending sort on zip has returned the expected zip code
+		assertEquals(lastZipNumber, firstZipReturned, "Pageable sort order failed.");
+
+		logPageInfo(profilePage);
+		long pagedTotalElements = profilePage.getTotalElements();
+
+
+		if (profilePage.hasNext()) {
+			Pageable pageable2 = profilePage.nextPageable();
+			Page<Profile> profilePageNext = profileRepository.findAll(pageable2);
+			logPageInfo(profilePageNext);
+		} else {
+			assertTrue(profilePage.hasNext(), "Pageable findAll(pageable) failed to return expected second page");
+		}
+
+		logger.debug("Paged Profile size {}, Non-paged size {}", () -> pagedTotalElements, () -> findAllTotalElements);
+		assertEquals(pagedTotalElements, findAllTotalElements, "Number of persisted findAll() Profiles differs with query by findAll(pageable)");
+	}
+
+	public static void logPageInfo(Page<Profile> profilePage) {
+		int number = profilePage.getNumber();
+		int numberOfElements = profilePage.getNumberOfElements();
+		int size = profilePage.getSize();
+		long totalElements = profilePage.getTotalElements();
+		int totalPages = profilePage.getTotalPages();
+		boolean hasNextPage = profilePage.hasNext();
+		logger.debug("Page info - page number {}, numberOfElements: {}, size: {}, "
+						+ "totalElements: {}, totalPages: {}, hasNextPage: {}",
+				number, numberOfElements, size, totalElements, totalPages, hasNextPage);
+
+		List<Profile> listOfProfiles = profilePage.getContent();
+
+		for (Profile p : listOfProfiles) {
+			logger.debug("Profile details: {}", p::toString);
+		}
+	}
+}
